5d5869b38c7a280e28ab8932bd49ee556556be07
anacmaciel <casmaciel96@gmail.com>
2022-09-04 15:00:52 -0300

    excluindo linha com importe service

--- src/main/java/com/catalisa/relacionamentoentretabelas/repository/BairroRepository.java
+++ src/main/java/com/catalisa/relacionamentoentretabelas/repository/BairroRepository.java
@@ -3,7 +3,6 @@ package com.catalisa.relacionamentoentretabelas.repository;
 import com.catalisa.relacionamentoentretabelas.model.BairroModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 @Repository
 public interface BairroRepository extends JpaRepository<BairroModel, Integer> {
---
+++ src/main/java/com/catalisa/relacionamentoentretabelas/service/BairroService.java
@@ -0,0 +1,14 @@
+package com.catalisa.relacionamentoentretabelas.service;
+
+import com.catalisa.relacionamentoentretabelas.repository.BairroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BairroService {
+
+@Autowired
+private BairroRepository bairroRepository;
+
+
+}
